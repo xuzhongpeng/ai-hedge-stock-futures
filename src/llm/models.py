1fab01ebcbfaae5f47e082c0ec21537e2bfdbd73
virattt <virat.dot@gmail.com>
2025-03-09 17:28:33 -0400

    Add gemini support

--- src/llm/models.py
+++ src/llm/models.py
@@ -1,5 +1,6 @@
 import os
 from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain_deepseek import ChatDeepSeek
@@ -11,9 +12,10 @@ from typing import Tuple
 
 class ModelProvider(str, Enum):
     """Enum for supported LLM providers"""
+    ANTHROPIC = "Anthropic"
+    GEMINI = "Gemini"
     OPENAI = "OpenAI"
     GROQ = "Groq"
-    ANTHROPIC = "Anthropic"
     DEEPSEEK = "Deepseek"
     QWEN = "QWen"
 
@@ -27,10 +29,18 @@ class LLMModel(BaseModel):
     def to_choice_tuple(self) -> Tuple[str, str, str]:
         """Convert to format needed for questionary choices"""
         return (self.display_name, self.model_name, self.provider.value)
-    
+
+    def has_json_mode(self) -> bool:
+        """Check if the model supports JSON mode"""
+        return not self.is_deepseek() and not self.is_gemini()
+
     def is_deepseek(self) -> bool:
         """Check if the model is a DeepSeek model"""
         return self.model_name.startswith("deepseek")
+
+    def is_gemini(self) -> bool:
+        """Check if the model is a Gemini model"""
+        return self.model_name.startswith("gemini")
 
 
 # Define available models
@@ -79,6 +89,16 @@ AVAILABLE_MODELS = [
         display_name="[groq] deepseek-r1 70b",
         model_name="deepseek-r1-distill-llama-70b",
         provider=ModelProvider.GROQ
+    ),
+    LLMModel(
+        display_name="[gemini] gemini-2.0-flash",
+        model_name="gemini-2.0-flash",
+        provider=ModelProvider.GEMINI
+    ),
+    LLMModel(
+        display_name="[gemini] gemini-2.0-pro",
+        model_name="gemini-2.0-pro-exp-02-05",
+        provider=ModelProvider.GEMINI
     ),
     LLMModel(
         display_name="[groq] llama-3.3 70b",
@@ -139,7 +159,6 @@ def get_model(model_name: str, model_provider: ModelProvider) -> ChatOpenAI | Ch
     elif model_provider == ModelProvider.DEEPSEEK:
         api_key = os.getenv("DEEPSEEK_API_KEY")
         if not api_key:
-            print(f"API Key Error: Please make sure DEEPSEEK_API_KEY is set in your .env file.")
             raise ValueError("Deepseek API key not found.  Please make sure DEEPSEEK_API_KEY is set in your .env file.")
         return ChatDeepSeek(model=model_name, api_key=api_key)
     elif model_provider == ModelProvider.QWEN:
@@ -148,3 +167,9 @@ def get_model(model_name: str, model_provider: ModelProvider) -> ChatOpenAI | Ch
             print(f"API Key Error: Please make sure QWEN_API_KEY is set in your .env file.")
             raise ValueError("QWen API key not found.  Please make sure QWEN_API_KEY is set in your .env file.")
         return ChatQWen(model=model_name, api_key=api_key)
+    elif model_provider == ModelProvider.GEMINI:
+        api_key = os.getenv("GOOGLE_API_KEY")
+        if not api_key:
+            print(f"API Key Error: Please make sure GOOGLE_API_KEY is set in your .env file.")
+            raise ValueError("Google API key not found.  Please make sure GOOGLE_API_KEY is set in your .env file.")
+        return ChatGoogleGenerativeAI(model=model_name, api_key=api_key)
